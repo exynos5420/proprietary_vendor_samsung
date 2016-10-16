4c9f810f1ef82a5d6b19c34795aa44795003d844
deadman96385 <seanhoyt963@gmail.com>
2016-10-16 15:46:28 -0500

    Make a chagall-common
    Change-Id: Ied883b34c93e785d2485834eec6713ce2236ac65

--- chagalllte/chagalllte-vendor-blobs.mk
+++ chagalllte/chagalllte-vendor-blobs.mk
@@ -20,23 +20,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/samsung/chagalllte/proprietary/vendor/firmware/bcm4350_V0301.0591.hcd:system/vendor/firmware/bcm4350_V0301.0591.hcd
 
-# WiFi - Firmware
-PRODUCT_COPY_FILES += \
-    vendor/samsung/chagalllte/proprietary/etc/wifi/nvram_mfg.txt_4354_a0:system/etc/wifi/nvram_mfg.txt_4354_a0 \
-    vendor/samsung/chagalllte/proprietary/etc/wifi/nvram_mfg.txt_4354_a1:system/etc/wifi/nvram_mfg.txt_4354_a1 \
-    vendor/samsung/chagalllte/proprietary/etc/wifi/nvram_net.txt_4354_a0:system/etc/wifi/nvram_net.txt_4354_a0 \
-    vendor/samsung/chagalllte/proprietary/etc/wifi/nvram_net.txt_4354_a1:system/etc/wifi/nvram_net.txt_4354_a1
-
 # Sensors
 PRODUCT_COPY_FILES += \
     vendor/samsung/chagalllte/proprietary/lib/hw/sensors.universal5420.so:system/lib/hw/sensors.universal5420.so
-
-# Camera
-PRODUCT_COPY_FILES += \
-    vendor/samsung/chagalllte/proprietary/lib/hw/camera.universal5420.so:system/lib/hw/camera.universal5420.so
-
-# GPS
-PRODUCT_COPY_FILES += \
-    vendor/samsung/chagalllte/proprietary/bin/gpsd:system/bin/gpsd \
-    vendor/samsung/chagalllte/proprietary/lib/libwrappergps.so:system/lib/libwrappergps.so \
-    vendor/samsung/chagalllte/proprietary/lib/hw/gps.default.so:system/lib/hw/gps.default.so
