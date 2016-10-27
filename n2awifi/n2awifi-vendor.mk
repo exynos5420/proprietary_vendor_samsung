163fc59ba25eafbead9639ebfae5c68b817f5f5e
deadman96385 <seanhoyt963@gmail.com>
2016-10-26 20:57:54 -0500

    Make n2awifi directly inherit from n1awifi
    No reason to just duplicate these when there is no changes
    
    Change-Id: Ie962b2f8fe6208d58b03a51634028acdb7f8a30a

--- n2awifi/n2awifi-vendor.mk
+++ n2awifi/n2awifi-vendor.mk
@@ -17,7 +17,7 @@
 # Pick up overlay for features that depend on non-open-source files
 DEVICE_PACKAGE_OVERLAYS := vendor/samsung/n2awifi/overlay
 
-$(call inherit-product, vendor/samsung/n2awifi/n2awifi-vendor-blobs.mk)
+$(call inherit-product, vendor/samsung/n1awifi/n1awifi-vendor-blobs.mk)
 $(call inherit-product, vendor/samsung/exynos5420-common/exynos5420-common-vendor.mk)
 
 # Include landscape camera blobs
