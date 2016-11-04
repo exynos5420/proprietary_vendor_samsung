349f831affa5ab25fc22fe1b4064897dfbeb68b5
Thomas Patry <thompatry@gmail.com>
2016-11-03 22:36:24 -0500

    Update sswap from T705XXU1ANF8 and commonize them for all Exynos 5420 devices
    Thank you deadman96385 <seanhoyt963@gmail.com> for tracking down the files
    
    Change-Id: I3c237f2c7f740cc4d98ee50d183367647e3e68c1

--- exynos5420-common/exynos5420-common-vendor-blobs.mk
+++ exynos5420-common/exynos5420-common-vendor-blobs.mk
@@ -32,6 +32,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/samsung/exynos5420-common/proprietary/lib/libstagefright_hdcp.so:system/lib/libstagefright_hdcp.so
 
+# SSWAP
+PRODUCT_COPY_FILES += \
+    vendor/samsung/exynos5420-common/proprietary/sbin/sswap:root/sbin/sswap
+
 # Widevine From T805XXU1CPH5
 PRODUCT_COPY_FILES += \
     vendor/samsung/exynos5420-common/proprietary/lib/liboemcrypto.so:system/lib/liboemcrypto.so \
