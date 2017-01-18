1408e32f38ae92eb5725d27361c484ee86d1dfb3
deadman96385 <seanhoyt963@gmail.com>
2017-01-18 01:01:33 -0600

    Import chagalllteub vendor
    Copy of chagalllte's layout just with chagalllteub blobs
    
    Change-Id: I61e8651eff4abd2c5a81c50292ff4bfda043991d

---
+++ chagalllteub/chagalllteub-vendor-blobs.mk
@@ -0,0 +1,30 @@
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# RIL
+PRODUCT_COPY_FILES += \
+    vendor/samsung/chagalllteub/proprietary/bin/cbd:system/bin/cbd \
+    vendor/samsung/chagalllteub/proprietary/lib/libsec-ril.so:system/lib/libsec-ril.so
+
+# Bluetooth firmware
+PRODUCT_COPY_FILES += \
+    vendor/samsung/chagalllteub/proprietary/vendor/firmware/bcm4350.hcd:system/vendor/firmware/bcm4350.hcd \
+    vendor/samsung/chagalllteub/proprietary/vendor/firmware/bcm4350_murata.hcd:system/vendor/firmware/bcm4350_murata.hcd \
+    vendor/samsung/chagalllteub/proprietary/vendor/firmware/bcm4350_semco.hcd:system/vendor/firmware/bcm4350_semco.hcd \
+    vendor/samsung/chagalllteub/proprietary/vendor/firmware/bcm4350_semcosh.hcd:system/vendor/firmware/bcm4350_semcosh.hcd \
+    vendor/samsung/chagalllteub/proprietary/vendor/firmware/bcm4350_semcove.hcd:system/vendor/firmware/bcm4350_semcove.hcd
+
+# Sensors
+PRODUCT_COPY_FILES += \
+    vendor/samsung/chagalllteub/proprietary/lib/hw/sensors.universal5420.so:system/lib/hw/sensors.universal5420.so
